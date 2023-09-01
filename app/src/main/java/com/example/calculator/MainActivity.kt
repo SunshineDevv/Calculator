202e82ea46566fcd3cde773da2c9e0d4e990b221
SunshineDevv <sasha_kolesnikov_2017@ukr.net>
2023-09-01 21:23:50 +0300

    Version 1.1 (Basic operations, clear back and new TextView with solved expression)

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.text.isDigitsOnly
 import com.example.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,39 +15,29 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 //        Log.i("Test","$number")
-        binding.buttonClearAll.setOnClickListener {
-            clearAll()
-        }
-        binding.button0.setOnClickListener {
-            setTextView("0")
-        }
-        binding.button1.setOnClickListener {
-            setTextView("1")
-        }
-        binding.button2.setOnClickListener {
-            setTextView("2")
-        }
-        binding.button3.setOnClickListener {
-            setTextView("3")
-        }
-        binding.button4.setOnClickListener {
-            setTextView("4")
-        }
-        binding.button5.setOnClickListener {
-            setTextView("5")
-        }
-        binding.button6.setOnClickListener {
-            setTextView("6")
-        }
-        binding.button7.setOnClickListener {
-            setTextView("7")
-        }
-        binding.button8.setOnClickListener {
-            setTextView("8")
-        }
-        binding.button9.setOnClickListener {
-            setTextView("9")
-        }
+        binding.buttonClearAll.setOnClickListener { clearAll() }
+        binding.buttonClear.setOnClickListener { clearBack() }
+        binding.button0.setOnClickListener { setTextView("0") }
+        binding.button1.setOnClickListener { setTextView("1") }
+        binding.button2.setOnClickListener { setTextView("2") }
+        binding.button3.setOnClickListener { setTextView("3") }
+        binding.button4.setOnClickListener { setTextView("4") }
+        binding.button5.setOnClickListener { setTextView("5") }
+        binding.button6.setOnClickListener { setTextView("6") }
+        binding.button7.setOnClickListener { setTextView("7") }
+        binding.button8.setOnClickListener { setTextView("8") }
+        binding.button9.setOnClickListener { setTextView("9") }
+        binding.buttonDelimiter.setOnClickListener { setTextView(".") }
+        binding.buttonAddition.setOnClickListener { setTextView("+") }
+        binding.buttonDivision.setOnClickListener { setTextView("/") }
+        binding.buttonSubtraction.setOnClickListener { setTextView("-") }
+        binding.buttonMultiplication.setOnClickListener { setTextView("×") }
+        binding.buttonSolve.setOnClickListener {
+        solveOperation()}
+        binding.buttonPercent.setOnClickListener { setTextView("%") }
+        binding.buttonFactorial.setOnClickListener { setTextView("!") }
+        binding.buttonExponentation.setOnClickListener { setTextView("^") }
+        binding.buttonPi.setOnClickListener { setTextView("3.14") }
     }
 
     private fun setTextView(str: String){
@@ -58,7 +50,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun clearAll(){
         binding.editText.text = "0"
+        binding.solvedOperationText.text = ""
     }
 
+    private fun clearBack(){
+        val str = binding.editText.text.toString()
+        var res = ""
+        if(str.isNotEmpty()){
+            res = if(str.length != 1 && str != "0") {
+                str.substring(0,str.length-1)
+            }else{
+                "0"
+            }
+        }
+        binding.editText.text = res
+    }
 
+    private fun solveOperation(){
+        var expression: String = binding.editText.text.toString()
+        val exp = ExpressionBuilder(expression).build()
+        val result = exp.evaluate()
+        binding.solvedOperationText.text = result.toString()
+    }
 }
+
