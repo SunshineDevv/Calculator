8972bc6f4f2cc0a89f08ee0810b649c943b6e896
SunshineDevv <sasha_kolesnikov_2017@ukr.net>
2023-09-01 17:09:53 +0300

    Version 1.0 (Number buttons & clear button)

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+//        Log.i("Test","$number")
+        binding.buttonClearAll.setOnClickListener {
+            clearAll()
+        }
+        binding.button0.setOnClickListener {
+            setTextView("0")
+        }
+        binding.button1.setOnClickListener {
+            setTextView("1")
+        }
+        binding.button2.setOnClickListener {
+            setTextView("2")
+        }
+        binding.button3.setOnClickListener {
+            setTextView("3")
+        }
+        binding.button4.setOnClickListener {
+            setTextView("4")
+        }
+        binding.button5.setOnClickListener {
+            setTextView("5")
+        }
+        binding.button6.setOnClickListener {
+            setTextView("6")
+        }
+        binding.button7.setOnClickListener {
+            setTextView("7")
+        }
+        binding.button8.setOnClickListener {
+            setTextView("8")
+        }
+        binding.button9.setOnClickListener {
+            setTextView("9")
+        }
     }
+
+    private fun setTextView(str: String){
+        if (binding.editText.text.length == 1 && binding.editText.text.toString() == "0"){
+            binding.editText.text = str
+        }else{
+            binding.editText.append(str)
+        }
+    }
+
+    private fun clearAll(){
+        binding.editText.text = "0"
+    }
+
+
 }
